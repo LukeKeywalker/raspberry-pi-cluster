02295ee753adcd6303be3ea581a2f6daea8f35b0
user <user@hackbook>
2021-04-21 21:27:39 +0200

    k8s firewall rules

--- first-boot/func/first-boot-func.sh
+++ first-boot/func/first-boot-func.sh
@@ -30,3 +30,60 @@ create_user()
 		$1
 	echo "$1:qwert" | chpasswd
 }
+
+add_master_node_firewall_rules()
+{
+	firewall-cmd --permanent --add-port=6443/tcp
+	firewall-cmd --permanent --add-port=2379-2380/tcp
+	firewall-cmd --permanent --add-port=10250/tcp
+	firewall-cmd --permanent --add-port=10251/tcp
+	firewall-cmd --permanent --add-port=10252/tcp
+	firewall-cmd --permanent --add-port=10255/tcp
+	firewall-cmd --reload
+}
+
+add_worker_node_firewall_rules()
+{
+	firewall-cmd --permanent --add-port=10251/tcp
+	firewall-cmd --permanent --add-port=10255/tcp
+	firewall-cmd --reload
+}
+
+update_iptable_settings()
+{
+	echo 'net.bridge.bridge-nf-call-ip6tables = 1' > /etc/sysctl.d/k8s.conf
+	echo 'net.bridge.bridge-nf-call-iptables = 1' >> /etc/sysctl.d/k8s.conf
+	sysctl --system
+}
+
+disable_swap()
+{
+	sed -i '/swap/d' /etc/fstab
+	swapoff -a
+}
+
+disable_selinux()
+{
+	setenforce 0
+	sed -i 's/^SELINUX=enforcing$/SELINUX=permissive/' /etc/selinux/config
+}
+
+update_cgroup_memory_settings()
+{
+	sed '$s/$/ cgroup_memory=1 cgroup_enable=memory/' /boot/cmdline.txt > cmdline.tmp
+	mv cmdline.tmp /boot/cmdline.txt
+}
+
+set_master_node_hostname()
+{
+	hostnamectl set-hostname master-node-$1
+}
+
+set_worker_node_hostname()
+{
+	hostnamectl set-hostname worker-node-$1
+}
+
+add_worker_nodes_hosts()
+{
+}
