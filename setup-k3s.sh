80a5433e7b8406d9f20e0fa9c549e1bab769bfbc
lmichniewicz <lukasz.michniewicz@gmail.com>
2021-10-12 17:39:19 +0200

    broken setup k3s script, restore defaults for container registry ingress

--- setup-k3s.sh
+++ setup-k3s.sh
@@ -2,11 +2,11 @@
 
 num_master_nodes=$1
 num_worker_nodes=$2
-rsa_pub_key=$3
+rsa_key=$3
 
 print_usage()
 {
-	echo "usage: $0 num_master_nodes num_worker_nodes" 
+	echo "usage: $0 num_master_nodes num_worker_nodes rsa_key" 
 }
 
 parse_arguments() 
@@ -25,7 +25,7 @@ parse_arguments()
 		exit 1
 	fi
 
-	if [ -z "${rsa_pub_key}" ] || [[ ! -f ${rsa_pub_key} ]]
+	if [ -z "${rsa_key}" ] || [[ ! -f ${rsa_key} ]]
 	then
 		print_usage
 		exit 1
@@ -34,36 +34,34 @@ parse_arguments()
 
 parse_arguments
 
+### FIXME! ###
+
 # install cluster with first master node at 192.168.1.11
-k3sup install --cluster \
-	--k3s-extra-args "--no-deploy traefik" \
-       	--ip 192.168.1.11 \
-	--user root \
-	--ssh-key ${rsa_pub_key}
+#k3sup install --cluster \
+#	--ip 192.168.1.11 \
+#	--k3s-extra-args "--no-deploy traefik" \
+#	--user root \
+#	--ssh-key ${rsa_key}
 
 # join rest of master nodes
 for ((node=2; node<=${num_master_nodes}; node++))
 do
-	node_ip="192.168.1.$((10+${node}))"
-	k3sup join --ip ${node_ip} \
-		--server \
-	        --k3s-extra-args "--no-deploy traefik" \
-		--user root \
-		--ssh-key ${rsa_pub_key} \
-		--server-user root \
-		--server-ip 192.168.1.11
+    node_ip="192.168.1.$((10+${node}))"
+    k3sup join --ip ${node_ip} \
+        --server \
+        --k3s-extra-args "--no-deploy traefik" \
+        --user root \
+        --ssh-key ${rsa_key} \
+        --server-user root \
+        --server-ip 192.168.1.11
 done
 
 # join worker nodes
 for ((node=1; node<=${num_worker_nodes}; node++))
 do
 	node_ip="192.168.1.$((10+${num_master_nodes}+${node}))"
-	k3sup join --ip ${node_ip} \ 
-	        --k3s-extra-args "--no-deploy traefik" \
-		--user root \
-		--ssh-key ${rsa_pub_key} \
-		--server-user root \
-		--server-ip 192.168.1.11
+    echo "Node: ${node_ip}"
+	k3sup join --ip ${node_ip} --user root --ssh-key ${rsa_key} --server-user root --server-ip 192.168.1.11 
 done
 
 # install kubeconfig
